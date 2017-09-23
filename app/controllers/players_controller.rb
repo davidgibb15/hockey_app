9b7f904264421f738d15f5ca5e7f6a0e2795b0c6
davidabgibb <davidgibb15@gmail.com>
2017-09-23 12:19:00 -0700

    debugging

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,7 @@
 class PlayersController < ApplicationController
   def show
   	@player =Player.first
-  	@games = Cgame.where(player_id: 1)
+  	@games = Igame.where(player_id: 1)
   	@goals=[1,2,3,4,5,6,7,8]
   end
   def new
