dd5a27c4fb6770f684864cd5ddb1cc5553c93b0f
davidabgibb <davidgibb15@gmail.com>
2017-09-23 11:34:47 -0700

    debugging

--- db/migrate/20170712061231_add_teams_to_cgames.rb
+++ db/migrate/20170712061231_add_teams_to_cgames.rb
@@ -1,6 +1,6 @@
 class AddTeamsToCgames < ActiveRecord::Migration[5.1]
   def change
-  	    add_reference :cgames, :home_team, foreign_key: true
-    	add_reference :cgames, :away_team, foreign_key: true
+  	    #add_reference :cgames, :home_team, foreign_key: true
+    	#add_reference :cgames, :away_team, foreign_key: true
   end
 end
