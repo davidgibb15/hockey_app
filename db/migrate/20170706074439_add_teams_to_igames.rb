5b72125c373ffad7a5329023d7df920484ccc1bb
davidabgibb <davidgibb15@gmail.com>
2017-09-30 11:12:25 -0700

    debugging

--- db/migrate/20170706074439_add_teams_to_igames.rb
+++ db/migrate/20170706074439_add_teams_to_igames.rb
@@ -1,6 +1,6 @@
 class AddTeamsToIgames < ActiveRecord::Migration[5.1]
   def change
-    add_reference :igames, :home_team, foreign_key: true
-    add_reference :igames, :away_team, foreign_key: true
+  	add_foreign_key :igames, :teams, column: :home_team, primary_key: :id
+    add_foreign_key :igames, :teams, column: :away_team, primary_key: :id
   end
 end
