ab770d5b4c2fc37cd658f309fd0980a0aea50e57
davidabgibb <davidgibb15@gmail.com>
2017-09-30 11:23:17 -0700

    debugging

--- db/migrate/20170712061231_add_teams_to_cgames.rb
+++ db/migrate/20170712061231_add_teams_to_cgames.rb
@@ -1,6 +1,6 @@
 class AddTeamsToCgames < ActiveRecord::Migration[5.1]
   def change
-  	add_foreign_key :igames, :teams, column: :home_team_id, primary_key: :id
-    add_foreign_key :igames, :teams, column: :away_team_id, primary_key: :id
+  	add_foreign_key :cgames, :teams, column: :home_team_id, primary_key: :id
+    add_foreign_key :cgames, :teams, column: :away_team_id, primary_key: :id
   end
 end
